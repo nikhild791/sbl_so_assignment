a34248f476963b46d82f8e3f1d3dba617bb2ddb9
admin <dubeynikhil791@gmail.com>
2025-11-09 20:35:54 +0530

    fixing for drizzle migrations

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -1,9 +1,11 @@
 // drizzle.config.ts
 import { defineConfig } from "drizzle-kit";
 import {env} from '@repo/config/index'
+import path from 'path';
+
 export default defineConfig({
   out: "./drizzle",
-  schema: "./src/schema/index.ts",
+  schema: path.resolve(__dirname, "./src/schema/index.ts"),
   dialect: "postgresql",
   dbCredentials: {
     url: env.DATABASE_URL!,
