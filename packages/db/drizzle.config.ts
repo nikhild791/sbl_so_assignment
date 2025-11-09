ff77b776e77616c3f5687974e88daf7fdeea0bdf
admin <dubeynikhil791@gmail.com>
2025-11-09 20:19:58 +0530

    migrating db

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "drizzle-kit";
 import {env} from '@repo/config/index'
 export default defineConfig({
   out: "./drizzle",
-  schema: "./dist/schema/index.ts",
+  schema: "./src/schema/index.ts",
   dialect: "postgresql",
   dbCredentials: {
     url: env.DATABASE_URL!,
